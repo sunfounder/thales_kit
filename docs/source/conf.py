9e365db5f796bd24e7c6ad23eb689b07bb4cd344
zhanzghiyong0410 <2556703570@qq.com>
2021-02-02 14:44:04 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, SunFounder'
 author = 'SunFounder'
 
 # The full version, including alpha/beta/rc tags
-release = '\\'
+release = ''
 
 
 # -- General configuration ---------------------------------------------------
